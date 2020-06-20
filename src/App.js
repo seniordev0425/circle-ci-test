e23e043c0ad55b28eff0088ffb76087e9411566c
SeniorDev0425 <senior.dev0425@outlook.com>
2020-06-20 10:18:38 -0700

    CI&CD test

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn ReactScriptss
+          Test CI & CD
         </a>
       </header>
     </div>
